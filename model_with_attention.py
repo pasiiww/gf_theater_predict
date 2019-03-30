3d992e8e4e3c916796162aa2ebca204dc2f999e6
robo <onehoho@hotmail.com>
2019-03-30 13:24:58 +0800

    update attention method

--- model_with_attention.py
+++ model_with_attention.py
@@ -5,9 +5,9 @@ import torch.nn.functional as F
 
 # IOS 安卓官服 B服数据
 data = [
-        [[32,24,44],[24,29,47],[24,41,35],[27,26,48],[31,29,40],[29,39,32],[26,32,41]],
-        [[31,26,44],[24,29,47],[23,40,36],[27,27,46],[29,36,35],[29,32,39],[38,29,33]],
-        [[34,23,43],[23,28,48],[23,38,39],[36,27,38],[23,42,35],[23,33,44],[34,33,33]]
+        [[32,24,44],[24,29,47],[24,41,35],[27,26,48],[31,29,40],[29,39,32],[26,32,41],[32,37,30],[26,35,39]],
+        [[31,26,44],[24,29,47],[23,40,36],[27.1,26.9,46],[29,36,35],[29,32,39],[38,29,33],[28,29,43],[23,44,33]],
+        [[34,23,43],[23,28,48],[23,38,39],[36,27,38],[23,42,35],[23,33,44],[34,33.1,32.9],[22,36,42],[26,39,35]]
         ]
 
 data = np.array(data)
@@ -19,22 +19,20 @@ class Attention(nn.Module):
         self.attn = nn.Linear(6, 1)
 
     def forward(self, x, key , value):
-        #  x:  1 * seq_len1 * 3
-        #  key: 1 * seq_len2 * 3
+        #  x:  batch_size * seq_len1 * 3
+        #  key: batch_size * seq_len2 * 3
 
-        batch_size = x.size(0)
         seq_len1 = x.size(1)
-        seq_len2 = key.size(0)
-        x = x.view(-1,seq_len1,1,3).repeat(1,1,seq_len2,1)
-        key = key.view(1,1,seq_len2,3).repeat(batch_size,seq_len1,1,1)
+        seq_len2 = key.size(1)
 
+        x = x.view(-1,seq_len1,1,3).repeat(1,1,seq_len2,1)
+        key = key.view(-1,1,seq_len2,3).repeat(1,seq_len1,1,1)
         x = torch.cat([x,key],-1)
         x = self.attn(x)
+
         x = F.softmax(x.squeeze(-1),dim=-1)
         # x: batch x seq1 x seq2 x 3
         # seq2 x 3
-        value = value.view(1,seq_len2,3).repeat(batch_size,1,1)
-
         x = torch.bmm(x,value)
         return x
 
@@ -44,28 +42,34 @@ class testNet(nn.Module):
     def __init__(self):
         super(testNet,self).__init__()
 
-        self.fc1 = nn.Linear(9,32)
-        self.lstm = nn.LSTM(32,32,1,batch_first=True)
-        self.fc2 = nn.Linear(32,3)
+        self.fc1 = nn.Linear(3,16)
+        self.lstm = nn.LSTM(16,32,1,batch_first=True)
+        self.fc2 = nn.Linear(38,16)
+        self.fc3 = nn.Linear(16, 3)
         self.atte = Attention()
 
     def forward(self, x , atte_parameter):
 
-        cat_dict = [x]
+        cat_dict = []
         for v in atte_parameter:
-            value = self.atte(x,v,v)
+            value = self.atte(x,v[:,:-1],v[:,1:])
             cat_dict.append(value)
-        x = torch.cat(cat_dict,-1)
+
         x = self.fc1(x)
         #print(x.shape)
         #x = x.view(batch_size,-1,16)
         x, _ = self.lstm(x)
+        cat_dict.append(x)
+
+        x = torch.cat(cat_dict,dim=-1)
         x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
         return x
 
 net = testNet()
-#optimizer = torch.optim.SGD(net.parameters(),lr = 0.001,momentum=0.9,weight_decay=1E-7)
-optimizer = torch.optim.Adam(net.parameters(),lr = 0.005,weight_decay=1E-7)
+#optimizer = torch.optim.SGD(net.parameters(),lr = 0.00001,momentum=0.9)
+optimizer = torch.optim.Adam(net.parameters(),lr = 0.01,weight_decay=1E-7)
 loss1 = torch.nn.MSELoss()
 
 
@@ -83,11 +87,8 @@ def train(batch,label,atte_key):
 
 def test(batch,label,atte_key):
     net.eval()
-    optimizer.zero_grad()
     reslut = net(batch,atte_key)
-    loss = loss1(reslut,label) # 和gt做MSE
-    loss2 = torch.sum(torch.abs(torch.sum(reslut,-1) -100)) #约束概率的和为100.。。。normaliza
-    loss = loss + 0.1 * loss2
+    loss = loss1(reslut,label)
     return loss.detach()
 
 save_name = "temp_model2.pkl"
@@ -99,37 +100,32 @@ input_data = data[:,:-1]
 y = data[:,1:]
 # 输入为前n-1天的，预测对应后一天的概率
 
-for epoch in range(0): # 训练的轮数 一般几百次次就收敛了
-    for i in range(3):
-        #input_datas = torch.Tensor([input_data[i]])
-        #ys = torch.Tensor([y[i]])
-        #atte_key = torch.Tensor([input_data[(i+1)%3],input_data[(i+2)%3]])
-
-        input_datas = torch.Tensor(input_data)
-        ys = torch.Tensor(y)
-        atte_key = torch.Tensor([input_data[(i+1)%3],input_data[(i+2)%3]])
-
-        step_loss = train(input_datas,ys,atte_key)
-        print(step_loss)
+atte_key = torch.Tensor([[input_data[1],input_data[2],input_data[0]],[input_data[2],input_data[0],input_data[1]]])
+input_datas = torch.Tensor(input_data)
+ys = torch.Tensor(y)
+for epoch in range(0):
+    step_loss = train(input_datas,ys,atte_key)
+    print(step_loss)
+    if step_loss < 0.1:
+        break
 
 if save_name:
     torch.save(net.state_dict(),save_name)
-
-input_datas = torch.Tensor(input_data)
-ys = torch.Tensor(y)
-atte_key = torch.Tensor([input_data[0],input_data[1]])
 print("test loss ", test(input_datas,ys,atte_key))
 
-#test_data = [[38,21,41],[26,27,47],[24,36,40],[34,30,36]] #渠道服
+test_data = [[38,21,41],[26,27,47],[24,36,40],[34,30,36],[24,37,39],[32,35,33],[30,26,44],[31.9,36,32.1],[24,32,44]] #渠道服
+
 #输出结果 0:IOS 1:安卓官服 2:安卓b服
 
 name = ["IOS ","安官","安B "]
 for i in range(3):
     test_data = data[i]
-
     data_ = np.array(test_data)
     input_datas = torch.Tensor([data_])
     #print(input_datas)
-    atte_key = torch.Tensor([input_data[(i + 1) % 3], input_data[(i + 2) % 3]])
+    atte_key = torch.Tensor(
+        [[input_data[(i+1)%3]], [input_data[(i+2)%3]]])
     reslut = net(input_datas,atte_key).detach().numpy()
     print(name[i],reslut[0][-1])
+    #print(name[i], reslut[0])
+    #print(reslut[0][:-1]-test_data[1:])
